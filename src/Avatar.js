b7e8447db067d9f485aa607faf1d8905ac978260
jackaylohano <jackaylohana@gmail.com>
2020-06-20 22:59:31 +0500

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -3,10 +3,14 @@ import './Avatar.css';
 import 'tachyons';
 
 function Avatar (props) {
-return <div className="avatarstyle ma3 bg-light-purple dib pa4" >
+return  <div> 
+        <div><h1> Welcome to the Avatar World!</h1></div>
+        <div className="avatarstyle tc ma3 bg-light-purple dib pa2" >
            <img src="https://joeschmoe.io//api/v1/Jack" alt="Avatar" />
-           <h1>{props.name} </h1>
-           <h2>{props.work}</h2>
+           <h1 className="tc">{props.name} </h1>
+           <h2 className="tc">{props.work}</h2>
+           <button>Subscribe</button>
+       </div>
        </div>
 
 }
