4d2258c20e49c49ffe36dd44db8df499facf84f7
jackaylohano <jackaylohana@gmail.com>
2020-06-20 21:20:37 +0500

    Updated Demo component

--- src/Demo.js
+++ src/Demo.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './Demo.css'
 
 
 
 function Demo ({name, age}) {
-    return <div>
+    return <div className="maindiv_style">
        <h1>Hello {name}! Your age is {age}. </h1> 
        <h2>Before ten years you were {age-10}</h2>
 </div>
-
 }
 
 export default Demo;
