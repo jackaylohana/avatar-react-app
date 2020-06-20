6d9b6d863411987f7f0dd7f7ed28269db9c3b553
jackaylohano <jackaylohana@gmail.com>
2020-06-20 22:53:14 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Demo from './Demo'
+import Demo from './Demo';
+import Avatar from './Avatar';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -10,7 +11,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Demo name="Jack" age={25}/>,
+     <Demo name="Jack" age={25}/>,
+     <Avatar id="1" name="Jack" work="Software Developer" />, 
+     <Avatar id="2" name="John" work="Web Developer"/>, 
+     <Avatar id="3" name="Sam" work="Frontend Developer"/>, 
+     <Avatar id="4" name="Rose" work="Backend Developer"/>, 
   </React.StrictMode>,
   document.getElementById('root')
 );
