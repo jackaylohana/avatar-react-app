ce48d6f34a838262426081e938634648615d3f5f
jackaylohano <jackaylohana@gmail.com>
2020-06-20 21:22:52 +0500

    Created Avatar component
    An API is used to fetch customs avatars from website joeschmoe.io

---
+++ src/Avatar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Avatar.css';
+import 'tachyons';
+
+function Avatar () {
+return <div className="avatarstyle" >
+           <img src="https://joeschmoe.io//api/v1/Jack" alt="Avatar" />
+           <h1 className="tc" > Jack Lohana</h1>
+           <h2>Software Developer</h2>
+       </div>
+
+}
+
+export default Avatar;
